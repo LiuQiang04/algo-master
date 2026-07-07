b51a3ebe74534d11f9c350289fa738ced6267946
LiuQiang04 <2655874169@qq.com>
2026-07-07 09:29:38 +0800

    refactor: change MainLayout to flex layout, remove sidebar margin offset

--- frontend/src/components/Layout/MainLayout.tsx
+++ frontend/src/components/Layout/MainLayout.tsx
@@ -10,17 +10,15 @@ export default function MainLayout() {
   const [sidebarOpen, setSidebarOpen] = useState(false);
 
   return (
-    <div className="min-h-screen flex flex-col bg-gray-50">
+    <div className="min-h-screen flex flex-col">
       <Header onSidebarToggle={() => setSidebarOpen(true)} />
       <div className="flex flex-1">
         <Sidebar open={sidebarOpen} onClose={() => setSidebarOpen(false)} />
-        <main className="flex-grow md:ml-[240px]" style={{ paddingBottom: 'var(--bottom-nav-height, 0px)' }}>
+        <main className="flex-grow min-w-0" style={{ paddingBottom: 'var(--bottom-nav-height, 0px)' }}>
           <Outlet />
         </main>
       </div>
-      <div className="md:ml-[240px]">
-        <Footer />
-      </div>
+      <Footer />
       <BottomNav />
     </div>
   );
